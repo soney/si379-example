d2f1afdba28b06a6483a75bf592a709db899cba7
Steve Oney <swloney@gmail.com>
2023-10-31 08:44:31 -0400

    x

---
+++ lectures/lecture-18/parcel_build_example/index.50a4715f.js
@@ -0,0 +1,3 @@
+let t=0;const e=document.querySelector("button");e.addEventListener("click",()=>{var c;t++,e.innerText=1===(c=t)?`You clicked the button ${c} time`:`You clicked the button ${c} times`});//# sourceMappingURL=index.50a4715f.js.map
+
+//# sourceMappingURL=index.50a4715f.js.map
